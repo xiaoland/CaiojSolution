f31af21cb9658fd80db85cdffcf094216060721d
Lanzhijiang <lanzhijiang@foxmail.com>
2020-09-25 20:33:35 +0800

    1048 first version

--- question_solution/first_time/1048.cpp
+++ question_solution/first_time/1048.cpp
@@ -9,7 +9,7 @@ struct record {
     int step = 0;
 };
 record list[410000], now;
-bool v[410000], find = false;
+bool find = false;
 int head, tail, require;
 
 bool is_repeat(record p) {
@@ -63,6 +63,8 @@ void bfs() {
                             }
 
                             if(is_repeat(now) == false) {
+                                now.bu[to].loss_s = now.bu[to].full - now.bu[to].now_volumn;
+                                now.bu[from].loss_s = now.bu[from].full - now.bu[from].now_volumn;
                                 now.step = list[head].step + 1; // 必须这么做，因为中间有双重for循环，只有在第一个那里才会更新now
                                 list[++tail] = now;
                                 if(is_end(now)) {
@@ -75,5 +77,34 @@ void bfs() {
                 }
             }
         }
+    }
+}
+
+int main() {
+    for(int i = 1; i<=3; i++) {
+        scanf("%d", &list[1].bu[i].full);
+        if(i == 1) {
+            list[1].bu[i].is_empty = false;
+            list[1].bu[i].is_full = true;
+            list[1].bu[i].now_volumn = list[1].bu[i].full;
+        }
+        else {
+            list[1].bu[i].is_empty = true;
+            list[1].bu[i].is_full = false;
+            list[1].bu[i].now_volumn = 0;
+        }
+        list[1].bu[i].loss_s = list[1].bu[i].full - list[1].bu[i].now_volumn;
+    }
+    scanf("%d", require);
+
+    list[1].step = 0;
+    bfs();
+    
+    if(find) {
+        printf("yes");
+        printf("%d", list[tail].step);
+    }
+    else {
+        printf("no");
     }
 }
