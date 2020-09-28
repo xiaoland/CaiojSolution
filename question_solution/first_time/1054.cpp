6601f857cd537ef4bbf5f7408f40b0f05a974069
Lanzhijiang <lanzhijiang@foxmail.com>
2020-09-28 19:27:59 +0800

    1054 update 1

--- question_solution/first_time/1054.cpp
+++ question_solution/first_time/1054.cpp
@@ -1,1 +1,31 @@
-#
+#include <cstdio>
+#include <cstring>
+struct num {
+    int a[1100];
+    int len;
+    num() {
+        len = 0;
+        memset(a, 0, sizeof(a));
+    }
+};
+num a;
+int n;
+char as[1100];
+
+int main() {
+    scanf("%d", &n);
+    scanf("%s", as+1);
+    a.len = strlen(as+1);
+
+    for(int i = 1; i<=a.len; i++) {
+        a.a[a.len+i-1] = as[i] - '0';
+    }
+
+    num c = multiply_x(a, n);
+
+    for(int i = c.len; i>=1; i--) {
+        printf("%d", c.a[i]);
+    }
+    printf("\n");
+    return 0;
+}
