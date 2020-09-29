4caee95bd00e3ce1b6f683a8843168dcf824c51e
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2020-09-29 13:57:38 +0800

    final version of 1056

--- question_solution/first_time/1056.cpp
+++ question_solution/first_time/1056.cpp
@@ -1,59 +1,50 @@
 // author: Lan_zhijiang
-// description: 背包2
+// description: 背包2：REWRITE
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
-using namespace std; // algorithm库一定要有这个 
-int n, an[110], tot[11000];
+using namespace std;
 bool f[11000];
+int tot[11000], a[110], n;
 
 int main() {
-	int len, x, sum;
+	int sum, len, x;
 	scanf("%d", &n);
 	
-	for(int i = 1;i <= n; i++) {
-//		for(len = 1; len<=110; len++) {  
-//			if(scanf("%d", &an[i][len]) == EOF) {
-//				i--;
-//				break;
-//			}
-//			if(an[i][len] == -1) {
-//				len--;
-//				break;
-//			}
-//		}
+	for(int i = 1; i<=n; i++) {
 		len = 0;
 		x = 0;
 		while(scanf("%d", &x) != EOF) {
 			if(x == -1) {
 				break;
 			}
-			an[++len] = x;
-		} 
+			a[++len] = x;
+		}
 		
+		sum = 0;
 		memset(f, false, sizeof(f));
 		f[0] = true;
-		sum = 0; // sum是每个序列的最大值，因此要重置
 		
 		for(int j = 1; j<=len; j++) {
 			for(int k = sum; k>=0; k--) {
 				if(f[k]) {
-					f[k+an[j]] = true;
-					sum = max(sum, k+an[j]);
+					f[k+a[j]] = true;
+					sum = max(sum, k+a[j]);
 				}
 			}
-		} 
-		for(int j = 0; j<=11000; j++) {
+		}
+		
+		for(int j = 0; j<=11000; j++) { // 从0开始 
 			if(f[j]) {
 				tot[j]++;
 			}
 		}
 	}
+	
 	for(int i = 11000; i>=0; i--) {
 		if(tot[i] == n) {
 			printf("%d\n", i);
 			break;
 		}
 	}
-	return 0;
-}
+} 
