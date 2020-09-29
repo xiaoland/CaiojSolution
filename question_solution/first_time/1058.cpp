5b164a77cfb1b1a4d02889ba6f677a5486813014
Lanzhijiang <lanzhijiang@foxmail.com>
2020-09-29 21:52:51 +0800

    add next action

--- question_solution/first_time/1058.cpp
+++ question_solution/first_time/1058.cpp
@@ -1,7 +1,7 @@
 // author: Lan_zhijiang
-// description: 含价值的填满型背包1
-// 一维数组版
-// 需要进一步动态输出理解
+// description: 含价值的填满型 完全 背包
+// 本题的关键在于宝石数量无限
+// 需要进一步动态输出理解(需先理解1057_2)
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
@@ -24,15 +24,15 @@ int main() {
     out[0] = 0;
 
     for(int i = 1; i<=n; i++) {
-        for(int j = ml; j>=v[i]; j--) {
-            
-            if(out[j-v[i]] != -1) {
+        for(int j = v[i]; j<=ml; j++) {
+            if (out[j - v[i]] != -1) {
                 out[j] = max(out[j], out[j - v[i]] + m[i]);
             }
         }
     }
+
     int maxv = 0;
-    for(int i = 1; i<=ml; i++) {  // 找最大值
+    for(int i = 1; i<=ml; i++) {
         maxv = max(maxv, out[i]);
     }
     printf("%d\n", maxv);
