87206694a175aea8a4bdc03a39213a09ed0d738f
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-13 20:53:16 +0800

    almost perfect solution of 1061

--- 1061.cpp
+++ 1061.cpp
@@ -5,16 +5,18 @@
 #include <cstring>
 #include <algorithm>
 
-int n;
+int t;
 char encoder[6][10] = {"one", "puton", "out", "output", "in", "input"};  // C艹真麻烦...这样都要二维数组
 char s[1000001];
-bool f[1000001];
+bool f[1000001], quit;
 
 bool compare(int a, int b) {
     
     int wait_compare_length = strlen(encoder[b]);
-    for (int i = 1; i <= wait_compare_length; i++) {
-        if (s[a-i] != encoder[b][i-1]) {
+    // printf("COMPARE: wcl: %d\n", wait_compare_length);
+    for (int j = 0; j < wait_compare_length; j++) {
+        // printf("COMPARE: j: %d\n", j);
+        if (s[a-j] != encoder[b][wait_compare_length-j-1]) {
             return false;
         }
     }
@@ -23,46 +25,37 @@ bool compare(int a, int b) {
 }
 
 int main() {
-    int t = 0;
     scanf("%d", &t);
 
     while(t > 0) {
         t--;
-        int count = 0;
         memset(f, false, sizeof(f));
-        // f[0] = true;
+        f[0] = true;
 
         scanf("%s", s+1);
         int len = strlen(s+1);
 
-        for (int i = 1; i<=n; i++) {
-            bool quit = false;
+        for (int i = 1; i<len+1; i++) {
 
             for (int k = 0; k<=5; k++) {
-                if (s[i] != encoder[k][strlen(encoder[k])-1]) {
-                    quit = true;
-                    break;
-                }
-                else {
+                quit = true;
+                if (s[i] == encoder[k][strlen(encoder[k])-1]) {
+                    quit = false;
                     if (i < strlen(encoder[k])) {
+                        // printf("MAIN: break because i<encoder[k]\n");
                         break;
                     }
                     else {
-                        if (compare(i, k)) {  // 这里是从i开始倒着比较的，不然f[len]就不可能等于true了(不过本题也可以count=6)
+                        if (compare(i, k)) {
                             if (f[i-strlen(encoder[k])]) {
-                                count++;
-                                if (count >= 6) {
-                                    quit = true;
-                                    break;
-                                }
                                 f[i] = true;  // 诶...反正你得上一个有了，你下面才行
                             }
                         }
                     }
                 }
-            }
-            if (quit) {
-                break;
+                else {
+                    // printf("MAIN: %c and %c\n", s[i], encoder[k][strlen(encoder[k])-1]);
+                }
             }
         }
 
