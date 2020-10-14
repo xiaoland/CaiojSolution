c36ac479e5c0d0dbb31e78dfe014f8fc87bed8bb
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-14 20:31:06 +0800

    Update 1062.cpp

--- question_solution/first_time/1062.cpp
+++ question_solution/first_time/1062.cpp
@@ -1,2 +1,68 @@
 // author: Lan_zhijiang
 // description: the solution of problem 1062
+ 
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+ 
+char s[101];
+int n, d[101];
+char encoder[101][101];
+bool f[101];
+ 
+bool compare(int a, int b) {
+    int l = strlen(encoder[b]) - 1;
+    while (l >= 0) {
+        if (s[a] != encoder[b][l]) {
+            return false;
+        }
+        l--;
+        a--;
+    }
+}
+ 
+int main() {
+    scanf("%s", s+1);
+    scanf("%d", &n);
+    for (int i = 1; i<=n; i++) {
+        scanf("%s", encoder[i]+1);
+    }
+     
+    memset(f, false, sizeof(f));
+    memset(d, 0x3f, sizeof(d));
+    f[0] = true;
+     
+    int l = strlen(s+1);
+    // printf("%d\n", l);
+     
+    for (int i = 1; i<l; i++) {
+        for (int j = 1; j<=n; j++) {
+            if (s[i] != encoder[j][strlen(encoder[j]+1)-1]) {
+                // printf("check point 1, s[i]: %c, encoder[j]: %d\n", s[i], encoder[j]+1);
+            }
+            else {
+                if (i+1 < strlen(encoder[j]+1)) {
+                    printf("check point 2\n");
+                }
+                else {
+                    if (compare(i, j)) {
+                        printf("check point 3, i: %d, j: %d\n", i, j);
+                        int c = i-strlen(encoder[j]+1);
+                        if (c < 0) {
+                            c+=1;
+                        }
+                        if (f[c]) {  // strlen char数组千万不要漏了+1 
+                            printf("check point 4\n");
+                            f[i] = true;
+                            d[i] = min(d[i], d[c] + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+     
+    printf("%d", d[l-1]);
+    return 0;
+}
