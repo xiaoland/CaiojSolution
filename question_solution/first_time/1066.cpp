e1f15a4a80836f240ec3b1975be13f95461a4db2
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-16 20:38:10 +0800

    new solution of 1066

--- question_solution/first_time/1066.cpp
+++ question_solution/first_time/1066.cpp
@@ -1,1 +1,31 @@
-// 
+// author: Lan_zhijiang
+// description: the solution of problem 1066
+// 求什么，f数组就记录什么，一切遍历等操作都为了填写f而服务
+// 如：求时间的最小值，那么通过计算出所有情况下的时间，记录在f中，最后输出的时候找到符合条件的就好了
+
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+double bw, length;
+int n;
+double sa[1001], wa[1001];
+double f[1001];
+
+void read_in() {
+    wa[0] = 0.0;
+
+    scanf("%lf%lf%d", &bw, &length, &n);
+    for (int i = 1; i<=n; i++) {
+        scanf("%lf%lf", &wa[i], &sa[i]);
+        sa[i]/=60.0;
+        wa[i]+=wa[i-1];
+    }
+}
+
+int main() {
+    read_in();
+
+    
+}
